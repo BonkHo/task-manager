c321d5603a424d2a401a8b79b9dc39f751e3ca1b
John Bae <john21225@gmail.com>
2021-10-21 12:09:38 -0700

    Task retrieval and creation controllers complete

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -7,35 +7,49 @@ const Task = require("../models/task");
 // app.patch('/api/v1/tasks/:id')  - PATCH (edit) a task by ID
 // app.delete('/api/v1/tasks/:id') - DELETE a task by ID
 
-const getAllTasks = (req, res) => {
-  res.send("All items from the file");
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({ tasks });
+	} catch (err) {
+		res.status(500).json({ msg: err });
+	}
 };
 
 const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({ task });
+	} catch (err) {
+		res.status(500).json({ msg: err });
+	}
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+	try {
+		const { id: taskId } = req.params;
+		const task = await Task.findOne({ _id: taskId });
+		if (!task) {
+			return res.status(404).json({ msg: `No task with id: ${taskId} found.` });
+		}
+		res.status(200).json({ task });
+	} catch (err) {
+		res.status(500).json({ msg: err });
+	}
 };
 
 const editTask = (req, res) => {
-  res.send("Edit a task based on the id");
+	res.send("Edit a task based on the id");
 };
 
 const deleteTask = (req, res) => {
-  res.send("Delete a task based on the id");
+	res.send("Delete a task based on the id");
 };
 
 module.exports = {
-  getAllTasks,
-  createTask,
-  getTask,
-  editTask,
-  deleteTask,
+	getAllTasks,
+	createTask,
+	getTask,
+	editTask,
+	deleteTask,
 };
